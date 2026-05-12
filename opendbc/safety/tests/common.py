8d6cef590d42aa753c2c8d14e0f04b1c8f325cec
conversun <conversun@gmail.com>
2026-05-12 12:04:07 +0800

    safety/tests: expand Mazda TX overlap exclusion for GEN1+TI

--- opendbc/safety/tests/common.py
+++ opendbc/safety/tests/common.py
@@ -910,7 +910,7 @@ class SafetyTest(SafetyTestBase):
               continue
             if attr.startswith('TestHyundaiCanfd') and current_test.startswith('TestHyundaiCanfd'):
               continue
-            if attr.startswith('TestMazdaGen2') and current_test.startswith('TestMazdaGen2'):
+            if attr.startswith('TestMazda') and current_test.startswith('TestMazda'):
               continue
             if {attr, current_test}.issubset({'TestHyundaiLongitudinalSafety', 'TestHyundaiLongitudinalSafetyCameraSCC', 'TestHyundaiSafetyFCEVLong'}):
               continue
